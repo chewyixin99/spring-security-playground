0768df56a57fb7e9dfff5f67ef9e71896ce3de8d
Chew Yi Xin <chewyixin1999@gmail.com>
2022-10-26 16:48:21 +0800

    update readme and remove some imports

--- jwt-demo/src/main/java/com/example/jwtdemo/jwttoken/JwtTokenService.java
+++ jwt-demo/src/main/java/com/example/jwtdemo/jwttoken/JwtTokenService.java
@@ -6,7 +6,6 @@ import java.util.stream.Collectors;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.token.TokenService;
 import org.springframework.security.oauth2.jwt.JwtClaimsSet;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
--- jwt-demo/src/main/resources/certs/README.md
+++ jwt-demo/src/main/resources/certs/README.md
@@ -7,3 +7,7 @@ PUkey
 <!-- prkey must be in pkcs8 format, following command have to be run -->
 PRKey
 - $ openssl pkcs8 -topk8 -inform PEM -outform PEM -nocrypt -in keypair.pem -out private.pem
+
+Download OPENSSL
+- https://slproweb.com/products/Win32OpenSSL.html
+- 2nd option win64 openssl v3.0.5 (or choose whichever is suitable for your device)
